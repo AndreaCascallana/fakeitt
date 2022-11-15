673059dac87932c81a61cce9804fcd85486507f2
deseo-grafico <deseografico.natalia@gmail.com>
2022-11-15 19:59:58 +0100

    Componente Post Card

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate,Link } from 'react-router-dom'
+import PostCard from '../components/Global/PostCard/PostCard'
 
 const Home = () => {
   //
@@ -45,41 +46,18 @@ const Home = () => {
       <div>Home</div>
       <div className='cardList'>
         {posts.map(post => 
-        <div key={post.id} className="postCard m-4 bg-slate-400 w-1/2">
-          <div className="cardData flex gap-1">
-            <div className="avatar"><img src="#" className="w-10"/></div>
-            <div className="author">{post.userId}</div>
-            <div className="date">{post.date}</div>
+        <>
+          <PostCard
+            key={post.id}
+            postId={post.id}
+            authorImg={post.userId.userImg}
+            postAuthor={post.userId}
+            postDate={post.date}
+            postText={post.text}
 
-          </div>
-          <div className="cardContent">
-            <div className="cardTitle">Static Title</div>
-            <div className="cardText">
-              {post.text}
-            </div>
-          </div>
-          <div className="postCtas flex gap-2 justify-end">
-            <div className="postCtaBlock flex gap-1">
-              <div className="icon w-4">C</div>
-              <div className="number">16</div>
-            </div>
-            <div className="postCtaBlock flex gap-1">
-              <div className="icon w-4">L</div>
-              <div className="number">16</div>
-            </div>
-            <div className="postCtaBlock flex gap-1">
-              <div className="icon w-4">S</div>
-              
-            </div>
+          /> 
           
-            
-
-          </div>
-          
-          <div>
-            <Link to={`/posts/${post.id}`}>Go to single post</Link>
-          </div>
-        </div>)}
+            </>)}
       </div>
     
     </>
