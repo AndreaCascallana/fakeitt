9584429d28aac78136d05bcbeedb55a38bba3012
deseo-grafico <deseografico.natalia@gmail.com>
2022-11-25 01:10:02 +0100

    (Fix): UseUserName

--- src/components/Global/useUserName.js
+++ src/components/Global/useUserName.js
@@ -11,7 +11,7 @@ const useUserName = () => {
       setUser(user_);
     } catch (e) {
       console.log(e);
-      userDeleted = "[deleted]";
+      const userDeleted = "[deleted]";
       return userDeleted;
     }
   };
