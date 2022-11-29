5177e3a573a71ff2e0b5d1c6662088695c15a4ff
AndreaCascallana <acascallana3016@esdmadrid.es>
2022-11-29 12:40:15 +0100

    (Fix): PostCard style conflicts

--- src/components/Global/PostCard/PostCard.jsx
+++ src/components/Global/PostCard/PostCard.jsx
@@ -19,7 +19,6 @@ import {
   icon,
   number,
   bottomContent,
-  singlePostNavi,
 } from "./PostCard.module.sass";
 
 const PostCard = ({ postId, authorImg, postAuthor, postDate, postText }) => {
@@ -34,52 +33,54 @@ const PostCard = ({ postId, authorImg, postAuthor, postDate, postText }) => {
 
   return (
     <Link to={`/post/${postId}`}>
-    <div className={postCard}>
-      <div className={cardData}>
-        <div className={avatarContainer}>
-          <div className={avatar}>
-            <img
-              src="https://images.unsplash.com/photo-1632765854612-9b02b6ec2b15?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80"
-              alt="User's Avatar"
-            />
+      <div className={postCard}>
+        <div className={cardData}>
+          <div className={avatarContainer}>
+            <div className={avatar}>
+              <img
+                src="https://images.unsplash.com/photo-1632765854612-9b02b6ec2b15?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80"
+                alt="User's Avatar"
+              />
+            </div>
+          </div>
+
+          <div className={dataContainer}>
+            <div className={author}>
+              {user.name} {user.fName}
+            </div>
+            <div className={divider}>-</div>
+            <div className={date}>{formatDate(postDate)}</div>
           </div>
         </div>
 
-        <div className={dataContainer}>
-          <div className={author}>
-            {user.name} {user.fName}
+        <div className={cardContent}>
+          <div className={cardTitle}>
+            <Link to={`/posts/${postId}`}>Static Title</Link>
           </div>
-          <div className={divider}>-</div>
-          <div className={date}>{formatDate(postDate)}</div>
-        </div>
-      </div>
-      
-      <div className={cardContent}>
-        <div className={cardTitle}><Link to={`/posts/${postId}`}>Static Title</Link></div>
-        <div className={cardText}>{postText}</div>
-      </div>
-
-      <div className={bottomContent}>
-        <div className={postCtas}>
-          <div className={postCtaBlock}>
-            <div className={icon}>Comments</div>
-            {/* ¿Cómo hallamos los numbers de cada CTA? */}
-            <div className={number}>16</div>
-          </div>
-          <div className={postCtaBlock}>
-            <div className={icon}>Likes</div>
-            <div className={number}>16</div>
-          </div>
-          <div className={postCtaBlock}>
-            <div className={icon}>Share</div>
-          </div>
+          <div className={cardText}>{postText}</div>
         </div>
 
-        {/* <div className={singlePostNavi}>
+        <div className={bottomContent}>
+          <div className={postCtas}>
+            <div className={postCtaBlock}>
+              <div className={icon}>Comments</div>
+              {/* ¿Cómo hallamos los numbers de cada CTA? */}
+              <div className={number}>16</div>
+            </div>
+            <div className={postCtaBlock}>
+              <div className={icon}>Likes</div>
+              <div className={number}>16</div>
+            </div>
+            <div className={postCtaBlock}>
+              <div className={icon}>Share</div>
+            </div>
+          </div>
+
+          {/* <div className={singlePostNavi}>
           <Link to={`/posts/${postId}`}>Go to single post</Link>
         </div> */}
+        </div>
       </div>
-    </div>
     </Link>
   );
 };
