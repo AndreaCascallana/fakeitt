a974620e0d186186b5b99394e4bdd797b6c17c59
AndreaCascallana <acascallana3016@esdmadrid.es>
2022-11-30 13:50:05 +0100

    (Fix): Better practices in tailwind color creation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,19 +4,21 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary900': '#cc2200',
-        'primary800': '#e33400',
-        'primary700': '#f13c00',
-        'primary': '#FF4500',
-        'primary500': '#ff4a05',
-        'primary400': '#ff6835',
-        'primary300': '#ff855b',
-        'primary200': '#ffa88a',
-        'primary100': '#ffcbb8',
-        'primary50': '#fee8e6',
+        'primary': {
+          900: '#cc2200',
+          800: '#e33400',
+          700: '#f13c00',
+          600: '#FF4500',
+          500: '#ff4a05',
+          400: '#ff6835',
+          300: '#ff855b',
+          200: '#ffa88a',
+          100: '#ffcbb8',
+          50: '#fee8e6',
+        }
       },
       fontFamily: {
-        body: ["IBM Plex Sans", "sans-serif"],
+        sans: ['"IBM Plex Sans"', "sans-serif"],
       },
       boxShadow: {
         'post': ['0px 3px 8px rgba(0, 0, 0, 0.16)'],
