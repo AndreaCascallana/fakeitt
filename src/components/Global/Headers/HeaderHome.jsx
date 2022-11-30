105c6a5618443702f65e928e0633955b35e98061
AndreaCascallana <acascallana3016@esdmadrid.es>
2022-11-30 18:30:25 +0100

    (Fix) Change Header and BottomNav padding

--- src/components/Global/Headers/HeaderHome.jsx
+++ src/components/Global/Headers/HeaderHome.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import Button from '../Button/Button';
-import{
-    header,
-    logo_container,
-    logo,
-    navi,
-    navItem
-
-} from './Header.module.sass'
+import React from "react";
+import Button from "../Button/Button";
+import {
+  header,
+  headerContainer,
+  logo_container,
+  logo,
+  navi,
+  navItem,
+} from "./Header.module.sass";
 import classNames from "classnames";
 import { NavLink } from "react-router-dom";
 
 const HeaderHome = () => {
-    const navLinkClasses = ({ isActive }) =>
+  const navLinkClasses = ({ isActive }) =>
     classNames({
-        "text-zinc-500": !isActive,
-        "text-black": isActive,
+      "text-zinc-500": !isActive,
+      "text-black": isActive,
     });
   return (
     <div className={header}>
+      <div className={headerContainer}>
         <div className={logo_container}>
           <NavLink className={(state) => navLinkClasses(state)} to="/">
             <img
@@ -31,13 +32,17 @@ const HeaderHome = () => {
 
         <nav className={navi}>
           <div className={navItem}>
-            <NavLink className={(state) => navLinkClasses(state)} to="/users/cea2fd15-b127-4062-b780-6fed8c7aa249">
-              <Button type='raw' icon='UserIcon'></Button>
+            <NavLink
+              className={(state) => navLinkClasses(state)}
+              to="/users/cea2fd15-b127-4062-b780-6fed8c7aa249"
+            >
+              <Button type="raw" icon="UserIcon"></Button>
             </NavLink>
           </div>
         </nav>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default HeaderHome
+export default HeaderHome;
